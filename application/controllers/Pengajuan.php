524f2432b0adc00c1833353b6ebea6191a0185d2
Muhammad Taftazani Adi <m.taftazani123@gmail.com>
2019-12-04 20:32:43 +0700

    Update REST API Pengajuan

--- application/controllers/Pengajuan.php
+++ application/controllers/Pengajuan.php
@@ -1,5 +1,4 @@
 <?php
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 require APPPATH . '/libraries/REST_Controller.php';
@@ -12,89 +11,19 @@ class Pengajuan extends REST_Controller {
         $this->load->database();
     }
 
-    //Menampilkan Jumlah Pengajuan
-    function count_get() {
-        $currentDate = date('Y/m/d');
-        $data = $this->db->query("SELECT COUNT(*) AS Jumlah FROM pengajuan WHERE tgl_pengajuan = '" . $currentDate . "' AND status = 'menunggu'");
-        $hasil = $data->row();
-        
-        echo $hasil->Jumlah;
-    }
-
-    //Menampilkan Data Pengajuan
-    // function transaksi()
-    // {
-    //     $currentDate = date('Y/m/d');
-    //     $status = "menunggu";
-        
-    //     if ($currentDate) 
-    //     {
-    //         $this->db->join('users.id', 'pengajuan.users');
-    //         $this->db->where('pengajuan.tgl_pengajuan', $currentDate);
-    //         $this->db->where('pengajuan.status', $status);
-    //         $data = $this->db->get('pengajuan')->result();
-    //     }
-    //     $this->response($data, 200);
-    // }
-
-    //Menampilkan data kontak
+    //Menampilkan data pemohon
     function index_get() {
-        // $id = $this->get('id');
-        // if ($id == '') {
-        //     $pengajuan = $this->db->get('pengajuan')->result();
-        // } else {
-        //     $this->db->where('id', $id);
-        //     $pengajuan = $this->db->get('pengajuan')->result();
-        // }
-        // $this->response($pengajuan, 200);
-
         $currentDate = date('Y/m/d');
         $status = "menunggu";
         
         if ($currentDate) 
         {
-            $data = $this->db->query("SELECT u.first_name, u.company FROM pengajuan p JOIN users u ON u.id = p.users WHERE p.status = '" . $status . "' AND p.tgl_pengajuan = '" . $currentDate . "'")->result();
+            $data = $this->db->query("SELECT u.first_name, u.company, COUNT(p.id) AS Jumlah
+                                      FROM pengajuan p 
+                                      JOIN users u ON u.id = p.users 
+                                      WHERE p.status = '" . $status . "' 
+                                      AND p.tgl_pengajuan = '" . $currentDate . "'")->result_array();
         }
         $this->response($data, 200);
     }
-
-    function index_post() {
-        $data = array(
-                    'id'           => $this->post('id'),
-                    'users'        => $this->post('users'),
-                    'id_barang'    => $this->post('id_barang'),
-                    'nim'          => $this->post('nim'),
-                    'qty'          => $this->post('qty'),
-                    'deskripsi'    => $this->post('deskripsi'),
-                    // 'tgl_pengajuan'=> $this->load->helper('date'),
-                    'tgl_pemakaian'=> $this->post('tgl_pemakaian'),
-                    'surat'        => $this->post('surat'),
-                    // 'status'       => $this->post('menunggu')
-                );
-        $insert = $this->db->insert('pengajuan', $data);
-        if ($insert) {
-            $this->response($data, 200);
-        } else {
-            $this->response(array('status' => 'fail', 502));
-        }
-    }
-
-    //Memperbarui data kontak yang telah ada
-    function index_put() {
-        $id = $this->put('id');
-        $data = array(
-                    'id'            => $this->put('id'),
-                    'users'         => $this->put('users'),
-                    'deskripsi'     => $this->put('deskripsi'));
-        $this->db->where('id', $id);
-        $update = $this->db->update('pengajuan', $data);
-        if ($update) {
-            $this->response($data, 200);
-        } else {
-            $this->response(array('status' => 'fail', 502));
-        }
-    }
-
-    //Masukan function selanjutnya disini
 }
-?>
