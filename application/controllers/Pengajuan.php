32db2d1c9fd96e6d7e68b7bcb860682208c3f071
Muhammad Taftazani Adi <m.taftazani123@gmail.com>
2019-12-05 19:47:11 +0700

    Finishing

--- application/controllers/Pengajuan.php
+++ application/controllers/Pengajuan.php
@@ -18,12 +18,26 @@ class Pengajuan extends REST_Controller {
         
         if ($currentDate) 
         {
-            $data = $this->db->query("SELECT u.first_name, u.company, COUNT(p.id) AS Jumlah
+            $data = $this->db->query("SELECT u.first_name, u.company
                                       FROM pengajuan p 
                                       JOIN users u ON u.id = p.users 
                                       WHERE p.status = '" . $status . "' 
                                       AND p.tgl_pengajuan = '" . $currentDate . "'")->result_array();
         }
         $this->response($data, 200);
+    }
+
+    function count_get() {
+        $currentDate = date('Y/m/d');
+        $status = "menunggu";
+
+        $this->db->select('count(*) as Jumlah');
+        $this->db->from('pengajuan');
+        $this->db->where('status', $status);
+        $this->db->where('tgl_pengajuan', $currentDate);
+        $data = $this->db->get();
+        $result = $data->result_array();
+
+        $this->response($result, 200);
     }
 }
